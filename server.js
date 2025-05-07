149bc8ff89fd7a2927cb204d6425e507f38195bc
HusainNit <vaintime21@gmail.com>
2025-05-07 18:16:22 +0300

    note in updatedTask route

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.get("/allTasks", async (req, res) => {
 });
 
 app.post("/updateTask", async (req, res) => {
+  //  can be used with params
   const updatetask = await Task.findByIdAndUpdate("680fc026c0b91825fbd4cddf", {
     isDone: true,
   });
